ad00f853aceb60812c9b47af157d98b0a311a762
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2021-04-01 07:57:24 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,13 +6,16 @@
 using namespace std;
 struct information
 {
-	string date = "";
+	string date = " ";
 	int amount = 0;
-	string detail_of_amount = "";
+	string detail_of_amount = " ";
 };
 void show_menu();
 void input_in_file(string, string);
-void display(string ,string );
+void display(string, string);
+string trim(string);
+
+//main function
 void main()
 {
 	int n = 1;
@@ -39,17 +42,16 @@ void show_menu()
 	cout << "\n    Enter: ";
 	cin >> num;
 	switch (num)
-
 	{
 	case 1:
 		//input debit in file
 		name = "Debit";
-		input_in_file(fileName,name);
+		input_in_file(fileName, name);
 		break;
 	case 2:
-		//add credit in file
+		//input credit in file
 		name = "Credit";
-		input_in_file(fileName,name);
+		input_in_file(fileName, name);
 		break;
 	case 3:
 		//output debit from file
@@ -57,6 +59,7 @@ void show_menu()
 		display(fileName, name);
 		break;
 	case 4:
+		//output credit from file
 		name = "Credit";
 		display(fileName, name);
 		break;
@@ -66,28 +69,23 @@ void show_menu()
 	}
 
 }
-//===========================
 
 //input function
-
-void input_in_file(string fileName, string Name )
+void input_in_file(string fileName, string Name)
 {
 	information x;
 	cin.ignore();
-	cout << "Enter Date for "<<Name<<" Amount: " << endl;
+	cout << "Enter Date for " << Name << " Amount: " << endl;
 	getline(cin, x.date);
-	//cin >> num1.date;
-	cout << "Enter "<< Name<<" Amount: " << endl;
+	cout << "Enter " << Name << " Amount: " << endl;
 	cin >> x.amount;
 	cin.ignore();
-	cout << "Enter Detail for "<< Name<<" Amount: " << endl;
+	cout << "Enter Detail for " << Name << " Amount: " << endl;
 	getline(cin, x.detail_of_amount);
-
 	// input in file
 	ofstream out(fileName, ios::app);
-	out <<Name<<"~" << x.date << "~" << x.amount << "~" << x.detail_of_amount << endl;
+	out << Name << "~" << x.date << "~" << x.amount << "~" << x.detail_of_amount << endl;
 	out.close();
-
 }
 
 //output function
@@ -104,62 +102,57 @@ void display(string fileName, string Name)
 		int i = 0;
 		char c = '\0';
 		while (c != '~') {
-
 			fileType += myText[i];
 			c = myText[i];
 			i++;
 		}
-
-		if (!fileType.empty()) {
-			fileType.resize(fileType.size() - 1);
+		fileType = trim(fileType);  //trim function call
+		string date = "";
+		c = '\0';
+		while (c != '~') {
+			date += myText[i];
+			c = myText[i];
+			i++;
 		}
-		
-			string date = "";
-			c = '\0';
-			while (c != '~') {
-				date += myText[i];
-				c = myText[i];
-				i++;
-			}
-			if (!date.empty()) {
-				date.resize(date.size() - 1);
-			}
-			string amount = "";
-			c = '\0';
-			while (c != '~') {
-				amount += myText[i];
-				c = myText[i];
-				i++;
-			}
-			if (!amount.empty()) {
-				amount.resize(amount.size() - 1);
-			}
-			stringstream ss;
-			ss << amount;
-			ss >> value;
-			if (fileType =="Credit" )
-			{
-				Total += value;
-			}
-			else if (fileType == "Debit") {
-				Total -= value;
-			}
-			string detail_of_amount = "";
-			for (i; i < myText.length(); i++) {
-				detail_of_amount += myText[i];
-			}
-			if (Name == fileType) {
-			cout  << setw(12) << date << setw(20) << amount << "               " << detail_of_amount << endl;
+		date = trim(date);   //trim function call
+		string amount = "";
+		c = '\0';
+		while (c != '~') {
+			amount += myText[i];
+			c = myText[i];
+			i++;
+		}
+		amount = trim(amount);   //trim function call
+		stringstream ss;
+		ss << amount;
+		ss >> value;
+		if (fileType == "Credit")
+		{
+			Total += value;
+		}
+		else if (fileType == "Debit") {
+			Total -= value;
+		}
+		string detail_of_amount = "";
+		for (i; i < myText.length(); i++) {
+			detail_of_amount += myText[i];
+		}
+		if (Name == fileType) {
+			cout << setw(12) << date << setw(20) << amount << "               " << detail_of_amount << endl;
 		}
 		else
 		{
 			continue;
 		}
 	}
-
-	
 	cout << "\n\n\n" << "Current Amount:" << Total << endl;
-
-
 	in.close();
+}
+
+//trim function
+ string trim(string entry) {
+	 if (!entry.empty()) {
+		 entry.resize(entry.size() - 1);
+	 }
+	 return entry;
 }
